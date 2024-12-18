01abc021f5ddd60e13eb90ee22d9404eb19a3a83
MarMarDonosti <mariamarques@tutamail.com>
2024-12-18 13:47:42 +0100

    recetas

--- Ejercicio2Postits/tareas.js
+++ Ejercicio2Postits/tareas.js
@@ -1,7 +1,7 @@
 //Identificar mis elementos de html
 const usuarioInput = document.getElementById('usuario');
 const taskInput = document.getElementById("task");
-const boton = document.getElementById ("create");
+const boton = document.getElementById("create");
 const fechaInput = document.getElementById('fecha');
 const tasksContainer = document.getElementById("board"); // va a ser el div
 
@@ -11,28 +11,28 @@ const tasksContainer = document.getElementById("board"); // va a ser el div
 var mis_tareas = JSON.parse(localStorage.getItem('mi_tarea')) || [];
 
 //Asigno un evento al boton, cuando lo pulso se cra la tarea
-boton.addEventListener('click', () =>{
+boton.addEventListener('click', () => {
     let usuario = usuarioInput.value;
     let text = taskInput.value.trim(); //trim: Para eliminar los espacios de delante y de detrás
     let fecha = fechaInput.value;                        // si el usuario los ha introducido
     console.log(text);
     //si tengo texto creo la tarea y si no no
-    if(text && fecha){
+    if (text && fecha) {
         //Crear la tarea
-        const newTask = {usuario, text, fecha, completed: false}; //completed: false para poder tachar la tarea /////////
+        const newTask = { usuario, text, fecha, completed: false }; //completed: false para poder tachar la tarea /////////
         mis_tareas.push(newTask);                                  //Cuando empiezo una nueva tarea, no está completada
- 
+
 
         //me invento una función para pintar mi array de tareas (las postit) en la web
         saveTasks()
         render_tasks()
-    }                   
+    }
     else {
         alert("Introduce la tarea")
-    }                             
+    }
 });
 
-function saveTasks () {
+function saveTasks() {
     localStorage.setItem('mi_tarea', JSON.stringify(mis_tareas));
 }
 
@@ -42,35 +42,35 @@ function render_tasks() {
     tasksContainer.innerHTML = ''; //Dejar vacío el contenedor (no tiene ningún postit pintado)
 
 
-    
+
     mis_tareas.forEach((element, index) => { //le añado el parámetro inventado (intuitivo) index
-                                            //nos indica su posición en el array
-                                            //hace referencia al índice interno del array
-                                           //primer parámetro: el contenido del array (element, es este caso)
-                                           //segundo parámetro: la posición de ese contenido (index, en este caso)
-        const existe = document.getElementById('postit_'+ element.usuario);
+        //nos indica su posición en el array
+        //hace referencia al índice interno del array
+        //primer parámetro: el contenido del array (element, es este caso)
+        //segundo parámetro: la posición de ese contenido (index, en este caso)
+        const existe = document.getElementById('postit_' + element.usuario);
         completa = "";
         console.log(element)
-        if (existe){
+        if (existe) {
             if (element.completed) {
                 completa = 'class="completed"' // task.completed ? class="completed" : ""  <-- esto es un if else en una misma línea
             }
-            
+
             existe.innerHTML += `<p ${completa} onclick="toogleTask(${index})">${element.text}</p><span>${element.fecha}</span><button class="btn-borrar"><img src="x-24.png" title="eliminar tarea"></button>`;
         } // += para que no sobreescriba sobre lo anterior
         else {
             //Si quiero añadir cosas:
             var new_postit = document.createElement('div');
             new_postit.className = "postit";
-            new_postit.id = "postit_"+ element.usuario;
+            new_postit.id = "postit_" + element.usuario;
             //Quiero que el backgroundcolor esté entre 4 definidos
-            new_postit.classList.add("color_"+(index%4)); //La siguiente línea que añade un id no sería necesaria
+            new_postit.classList.add("color_" + (index % 4)); //La siguiente línea que añade un id no sería necesaria
 
-            new_postit.id = "postit_"+element.usuario; //Cada postit tendrá un id, se asignará id automáticamente añadiéndole número a postit_
+            new_postit.id = "postit_" + element.usuario; //Cada postit tendrá un id, se asignará id automáticamente añadiéndole número a postit_
             if (element.completed) {
                 completa = 'class="completed"' // task.completed ? class="completed" : ""  <-- esto es un if else en una misma línea
             }
-            
+
             // ahora lo añado al contenedor
             new_postit.innerHTML = `
                 <div class="contenedor_circulo">
@@ -89,9 +89,9 @@ function render_tasks() {
     });
 }
 // para tachar las tareas
-function toogleTask(indice){
-    mis_tareas[indice].completed =!mis_tareas[indice].completed;
-   
+function toogleTask(indice) {
+    mis_tareas[indice].completed = !mis_tareas[indice].completed;
+
     render_tasks();
 }
 
@@ -100,9 +100,9 @@ function toogleTask(indice){
 render_tasks()
 
 // Botón de limpiar
-const boton2 = document.getElementById ("limpiar");
+const boton2 = document.getElementById("limpiar");
 
-boton2.addEventListener('click', () =>{
+boton2.addEventListener('click', () => {
     //localStorage.clear();  // Limpiar el almacenamiento local entero
     localStorage.removeItem('mi_tarea');
     mis_tareas = []; // Vaciar el array de tareas
